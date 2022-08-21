3f47a7aac391725f16999be7ac137f330458679b
Mercy <mercycheruiyot2013@gmail.com>
2022-08-21 16:34:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName ="bob";
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+return customerName
+}
+
+//console.log(customerName);
+
+//second question
+var bestCustomer //declaring a undefined variable in global scope.
+function setBestCustomer(){
+   return bestCustomer="not bob"
+   
+}
+//setBestCustomer(bestCustomer);
+
+//question 4 where the function overwrites best customer.
+function overwriteBestCustomer(){
+return bestCustomer="maybe bob"
+
+}
+//overwriteBestCustomer();
+
+//question 5 trying to reassign const variable
+const leastFavoriteCustomer="poorus";
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer='sam';  
+}
+changeLeastFavoriteCustomer();
